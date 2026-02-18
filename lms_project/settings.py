2dedaa075253450e36ebd451619be3b1ce89faf1
unknown <kowsalya2025>
2026-02-18 21:29:39 +0530

    Updated gitignore

--- lms_project/settings.py
+++ lms_project/settings.py
@@ -121,16 +121,17 @@ WSGI_APPLICATION = 'lms_project.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST", "localhost"),
-        'PORT': os.getenv("DB_PORT", "3306"),
-        'OPTIONS': {
-    'ssl': {'ssl_disabled': False},
-},
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": os.environ.get("DB_PORT", "3306"),
+        "OPTIONS": {
+            "ssl": {"ssl_disabled": False},
+            "charset": "utf8mb4",
+        },
     }
 }
 
